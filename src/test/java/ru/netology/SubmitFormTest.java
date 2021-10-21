efbe770cb763aee6086e55eb192275d70bd24b07
AlexeyPotapenko <alesha_potapenko@mail.ru>
2021-10-21 17:36:15 +0300

    setting CI

---
+++ src/test/java/ru/netology/SubmitFormTest.java
@@ -0,0 +1,56 @@
+package ru.netology;
+
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.List;
+
+class CallbackTest {
+    private WebDriver driver;
+
+
+    @BeforeAll
+    static void setUpAll() {
+        System.setProperty("webdriver.chrome.driver", "./driver/win/chromedriver.exe");
+
+    }
+
+    @BeforeEach
+    void setUp() {
+        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+
+
+    }
+
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+        driver = null;
+    }
+
+    @Test
+    void shouldTestV1() {
+        driver.get("http://localhost:9999");
+        List<WebElement> inputField = driver.findElements(By.className("input__control"));
+        inputField.get(0).sendKeys("Алексей");
+        inputField.get(1).sendKeys("+79174725035");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.tagName("Button")).click();
+
+        String actualMessage = driver.findElement(By.className("paragraph")).getText();
+        String expectedMessage = "  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        Assertions.assertEquals(expectedMessage, actualMessage);
+
+    }
+
+}
+

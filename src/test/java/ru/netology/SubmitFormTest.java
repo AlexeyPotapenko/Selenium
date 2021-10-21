8b1ee52145873b23d2cd1656dbfbfbc62c4d73eb
AlexeyPotapenko <alesha_potapenko@mail.ru>
2021-10-21 18:56:47 +0300

    add web driver manager

--- src/test/java/ru/netology/SubmitFormTest.java
+++ src/test/java/ru/netology/SubmitFormTest.java
@@ -1,11 +1,17 @@
 package ru.netology;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 
 import java.util.List;
 
@@ -16,7 +22,7 @@ class CallbackTest {
 
     @BeforeAll
     static void setUpAll() {
-        System.setProperty("webdriver.chrome.driver", "./driver/win/chromedriver.exe");
+        WebDriverManager.chromedriver().setup();
 
 
     }
@@ -35,9 +41,10 @@ class CallbackTest {
     }
 
     @AfterEach
-    void tearDown() {
-        driver.quit();
-        driver = null;
+    void teardown() {
+        if (driver != null) {
+            driver.quit();
+        }
     }
 
     @Test
